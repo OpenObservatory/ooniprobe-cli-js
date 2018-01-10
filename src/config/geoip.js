a54bdeeddb688a5bd70795b156b22e62d8e17f04
Arturo Filastò <arturo@filasto.net>
2018-01-10 13:12:11 +0100

    Add functions to download geoip data

---
+++ src/config/geoip.js
@@ -0,0 +1,69 @@
+import * as fs from 'fs-extra'
+import path from 'path'
+
+import axios from 'axios'
+import zlib from 'zlib'
+
+import { getOoniDir } from './global-path'
+
+const BASE_URL = 'https://github.com/OpenObservatory/ooni-resources/releases/download/'
+const LATEST_VERSION = '21'
+const GEOIP_ASN_FILENAME = 'maxmind-geoip.GeoIPASNum.dat'
+const GEOIP_COUNTRY_FILENAME = 'maxmind-geoip.GeoIP.dat'
+
+const downloadFile = ({url, dst, uncompress}) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'get',
+      url: url,
+      responseType: 'stream'
+    }).then(response => {
+      let outputStream
+      if (uncompress) {
+        outputStream = zlib.createGunzip()
+        outputStream.pipe(fs.createWriteStream(dst))
+        response.data.pipe(outputStream)
+      } else {
+        outputStream = response.data
+        outputStream.pipe(fs.createWriteStream(dst))
+      }
+      outputStream.on('end', () => {
+        resolve()
+      })
+    })
+  })
+}
+
+export const getGeoipPaths = async () => {
+  const geoipDir = path.join(getOoniDir(), 'geoip')
+
+  // XXX exception handling
+  const geoipDirExists = await fs.pathExists(geoipDir)
+  if (!geoipDirExists) {
+    await fs.ensureDir(geoipDir)
+  }
+
+  const geoipCountryPath = path.join(geoipDir, GEOIP_COUNTRY_FILENAME)
+  const geoipCountryExists = await fs.pathExists(geoipCountryPath)
+  if (!geoipCountryExists) {
+    await downloadFile({
+      url: `${BASE_URL}${LATEST_VERSION}/${GEOIP_COUNTRY_FILENAME}.gz`,
+      dst: geoipCountryPath,
+      uncompress: true
+    })
+  }
+
+  const geoipAsnPath = path.join(geoipDir, GEOIP_ASN_FILENAME)
+  const geoipAsnExists = await fs.pathExists(geoipAsnPath)
+  if (!geoipAsnExists) {
+    await downloadFile({
+      url: `${BASE_URL}${LATEST_VERSION}/${GEOIP_ASN_FILENAME}.gz`,
+      dst: geoipAsnPath,
+      uncompress: true
+    })
+  }
+  return {
+    countryPath: geoipCountryPath,
+    asnPath: geoipAsnPath,
+  }
+}
